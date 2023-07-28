1eb8f26e88a81890884d1a90f8a2456c30692818
AlejandroAngeles9803 <112768600+AlejandroAngeles9803@users.noreply.github.com>
2023-07-28 16:18:40 -0600

    💄 Cambios en las vistas

--- templates/views/departments/departmentsView.php
+++ templates/views/departments/departmentsView.php
@@ -1,3 +1,75 @@
 <?php require_once INCLUDES.'inc_header.php';?>
 
-<h1>Hola</h1>
+<div class="container-fluid mt-4">
+    <h1 class="text-white display-1 text-center">Departamentos</h1>
+
+    <h4 class="text-white text-center  mt-3">Ponemos la salud, la comodidad y el bienestar de nuestros pacientes como
+        nuestra principal prioridad</h4>
+</div>
+
+<div class="grid gap-5 mt-5 ">
+    <div class="p-2 g-col-6 opt-1 w-25 bg-light border rounded">
+        <div class="container-main">
+            <img class="img-thumbnail float" src="../Proyecto_Hospital\assets\images\icons8-corazón-con-pulso-100.png"
+            alt="...">
+        </div>
+
+        <h1 class="mt-4">Cardiología</h1>
+        <p>Nuestra clínica cuenta con un departamento de cardiología especializado y equipado con las últimas
+            tecnologías para garantizar un tratamiento completo y de calidad a nuestros pacientes.</p>
+        <button class="btn">
+            <span class="blockquote ">Agenda una cita</span>
+            <i class="fa-solid fa-arrow-right"></i>
+        </button>
+    </div>
+
+    <div class="p-2 g-col-6 opt-1 w-25 bg-light border rounded ">
+        <div class="container-main">
+            <img class="img-thumbnail float" src="../Proyecto_Hospital\assets\images\icons8-salud-mental-64.png"
+                alt="...">
+        </div>
+
+        <h1 class="mt-4">Neurología</h1>
+        <p>El departamento de neurología es el mejor para diagnosticar y tratar incluso las condiciones neurológicas más
+            difíciles.</p>
+        <button class="btn">
+            <span class="blockquote ">Agenda una cita</span>
+            <i class="fa-solid fa-arrow-right"></i>
+        </button>
+    </div>
+
+    <div class="p-2 g-col-6 opt-1 w-25 bg-light border rounded">
+        <div class="container-main">
+            <img class="img-thumbnail float" src="../Proyecto_Hospital\assets\images\icons8-pastillas-100.png"
+                alt="...">
+        </div>
+        <h1 class="mt-4">Farmacia</h1>
+        <p >Nuestro departamento farmacéutico, le brindara las mejores preinscripciones para
+        sus tratamientos.</p>
+        <button class="btn mt-4">
+            <span class="blockquote ">Agenda una cita</span>
+            <i class="fa-solid fa-arrow-right"></i>
+        </button>
+    </div>
+
+</div>
+
+<style>
+.grid {
+    display: flex;
+    justify-content: center;
+
+}
+
+.container-main {
+    float: inline-start;
+    position: absolute;
+    top: 27%;
+    width: 4.2vw;
+
+}
+
+.float {}
+</style>
+
+<?php require_once INCLUDES.'inc_footer.php';?>
--- templates/views/services/servicesView.php
+++ templates/views/services/servicesView.php
@@ -12,7 +12,7 @@
     </button>
   </div>
   <div class="p-2 g-col-6 opt-2 text-white w-100 h-100">
-  <img src="\Proyecto_Hospital\assets\images\icons8-doctor-male-skin-type-3-96.png" alt="">
+  <img class="img" src="\Proyecto_Hospital\assets\images\icons8-doctor-male-skin-type-3-96.png" alt="">
     <h3 class="display-3 mt-3 ">Doctores</h3>
     <p class="blockquote">Trabajamos y compartimos cooperación con los mejores expertos y los médicos más cuidadosos del país. Consulte la lista de nuestros médicos para ponerse en contacto fácilmente y reservar una cita.</p>
     
@@ -23,7 +23,7 @@
   </div>
 
   <div class="p-2 g-col-6 opt-1 text-white w-100 h-100">
-  <img src="\Proyecto_Hospital\assets\images\icons8-music-robot-96.png" alt="">
+    <img class="img" src="\Proyecto_Hospital\assets\images\icons8-music-robot-96.png" alt="">
     <h3 class="display-3 mt-3 ">Chat-Bot</h3>
     <p class="blockquote">Nuestro Chat-Bot de Consejos de Enfermería está disponible las 24 horas del día, los 7 días de la semana para casos de emergencia.</p>
     
@@ -51,7 +51,12 @@
         background-color: #535063;
         border: 1px solid #535063;
         margin: 10px;
-        height: 1000px;
+
+    }
+
+    .img{
+
+      width: 10vw;
 
     }
 </style>
